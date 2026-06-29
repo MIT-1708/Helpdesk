1d580e14c74e093111f12d2c16b0398baf675a5c
allle <allle@example.com>
2026-06-29 15:51:29 +0530

    initial 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import prisma from './prisma.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
